690d593f523b0ebac5f083febab8e39aecbf1523
Vladimir Pankov <wowofbob@gmail.com>
2015-09-10 05:24:24 +0300

    Made neuron to be parametrized by Responce value for Activation function and by Activation function. It looks cool that any neuron may have it's own how to eval things. But I'll have to set such unique params for any neuron in network. And, this params won't change during training. So there is no need for them in Neuron data structure.

--- main.hs
+++ main.hs
@@ -4,9 +4,9 @@ import qualified Data.Vector as DV
 type Input = Float
 type Weight = Float
 
-data Conn =
-  { connWeight :: Weight
-  , connSignal :: Input }
-
+-- Every neuron is parametrized. For first time.
 data Neuron =
-  Neuron { neuronInput :: DV.Vector Conn }
+  Neuron { neuronInputs     :: DV.Vector Double
+         , neuronThreshold  :: Double
+         , neuronResponse   :: Double
+         , neuronActivation :: Double -> Double }
