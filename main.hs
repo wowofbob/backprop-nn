1ee55cff3df66c9a642da8ee49bb18c9b08fd772
Vladimir Pankov <wowofbob@gmail.com>
2015-09-10 05:56:56 +0300

    Removed parameterization from Neuron. Maybe Layer to be parametrized. Found that with this structure I don't have connections between layers. It's not simple here

--- main.hs
+++ main.hs
@@ -1,12 +1,37 @@
 
 import qualified Data.Vector as DV
 
-type Input = Float
-type Weight = Float
+-------
+-- Auxiliaies ...
 
--- Every neuron is parametrized. For first time.
+dotProd :: Num a => DV.Vector a -> DV.Vector a -> a
+dotProd x = DV.sum . DV.zipWith (*) x
+
+sigmoid :: Floating a => a -> a -> a         
+sigmoid p x = 1 / ( 1 + exp ((-x)/p) )
+
+-------
+
 data Neuron =
-  Neuron { neuronInputs     :: DV.Vector Double
-         , neuronThreshold  :: Double
+  Neuron { neuronInputs    :: DV.Vector Double
+         , neuronThreshold :: Double }
+        
+type Activation = Double -> Double -> Double
+        
+sumInput :: Neuron -> DV.Vector Double -> Double
+sumInput n is = dotProd (neuronInputs n) is - (neuronThreshold n)
+
+activate :: Double -> Activation -> Neuron -> DV.Vector Double -> Double
+activate response f n = f response . sumInput n
+
+-- Layer is parametrized. It's better them parametrize neurons.
+data Layer =
+  Layer { layerNeurons    :: DV.Vector Neuron
+        , layerResponse   :: Double
+        , layerActivation :: Activation } 
+
+
+
+{-
          , neuronResponse   :: Double
-         , neuronActivation :: Double -> Double }
+         , neuronActivation :: Double -> Double }-}
